05083e28881413200bf55ad08ebf93b0ada94b68
khalid dahir <oblitrium@gmail.com>
2020-10-27 22:29:17 -0500

    Implemented advanced logger
    Implemented advanced logger with custom log format.
    The logger also log to the EditorLog which is located in Asset->Log

---
+++ TechDemoProject/Assets/Scripts/Addressable/AssetAddress.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using ZLogger;
+using Object = UnityEngine.Object;
+
+namespace Tech.Core
+{
+    public static class AssetAddress
+    {
+        private static readonly Microsoft.Extensions.Logging.ILogger Logger = LogManager.GetLogger("AssetLogger");
+        
+        public static async UniTaskVoid CreateAssetList<T>(AssetReference assetReference,
+            IList<T> objects,
+            IProgress<float> progress = null,
+            CancellationToken cancellationToken = default(CancellationToken))
+            where T : Object
+        {
+            if (assetReference.editorAsset != null || assetReference.Asset != null)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Logger.ZLogError("Cancellation Exception: The AssetAddress Retrieval Operation was canceled.");
+                    return;
+                }
+
+                objects.Add( await assetReference.InstantiateAsync()
+                    .ToUniTask(progress, PlayerLoopTiming.Update, cancellationToken) as T);
+            }
+            else
+            {
+                Logger.ZLogInformation("AssetReference Asset is Null. \nCan't load nothing from the AssetAddress."); 
+            }
+        }
+
+        public static async UniTaskVoid CreateAssetList<T>(IList<AssetReference> assetReferences,
+            IList<T> objects,
+            IProgress<float> progress = null,
+            CancellationToken cancellationToken = default(CancellationToken))
+        where T : Object
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                Logger.ZLogError("Cancellation Exception: The AssetAddress Retrieval Operation was canceled.");
+                return;
+            }
+
+            foreach (AssetReference assetReference in assetReferences)
+            {
+                if (assetReference.Asset != null || assetReference.editorAsset != null)
+                {
+                    objects.Add(await assetReference
+                        .InstantiateAsync()
+                        .ToUniTask(progress, PlayerLoopTiming.Update, cancellationToken) as T);   
+                }
+                else
+                {
+                    Logger.ZLogInformation("Current index AssetReference is empty (null) \n Can't load nothing from the AssetAddress.");
+                }
+            }
+        }
+        
+        public static async UniTaskVoid GetAllLocation(string label,
+            IList<IResourceLocation> loadedLocation,
+            IProgress<float> progress = null,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                Logger.ZLogError("Cancellation Exception: The AssetAddress Retrieval Operation was canceled");
+            }
+
+            IList<IResourceLocation> unloadLocation = await Addressables.LoadResourceLocationsAsync(label)
+                .ToUniTask(progress, PlayerLoopTiming.Update, cancellationToken);
+
+            foreach (var location in unloadLocation)
+            {
+                loadedLocation.Add(location);
+            }
+        }
+
+        public static async UniTaskVoid LoadByLocation<T>(IList<IResourceLocation> resourceLocations, IList<T> objects)
+        where T : Object
+        {
+            if (resourceLocations.Count <= 0)
+            {
+                Logger.ZLogInformation("resourceLocation list is empty");
+            }
+            
+            foreach (IResourceLocation location in resourceLocations)
+            {
+                objects.Add(await Addressables.InstantiateAsync(objects) as T);
+            }
+        }
+
+        public static async UniTaskVoid LoadByNameOrLabel<T>(string nameOrLabel,
+            IList<T> objects,
+            IProgress<float> progress = null,
+            CancellationToken cancellationToken = default(CancellationToken))
+        where T : Object
+        {
+            IList<IResourceLocation> resourceLocations= await Addressables.LoadResourceLocationsAsync(nameOrLabel).ToUniTask();
+
+           foreach (IResourceLocation location in resourceLocations)
+           {
+               objects.Add(await Addressables.InstantiateAsync(location).ToUniTask(progress, PlayerLoopTiming.Update, cancellationToken) as T);
+           }
+        }
+
+        public static void Release(IList<Object> objects)
+        {
+            foreach (var o in objects)
+            {
+                Addressables.Release(o);
+            }
+        }
+        
+        public static void Release(Object obj)
+        {
+            Addressables.Release(obj);
+        }
+    }
+}
---
+++ TechDemoProject/Assets/Scripts/Logger/LogManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection;
+using Cysharp.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using UniRx;
+using UnityEngine;
+using ZLogger;
+
+namespace Tech.Core
+{
+    public static class LogManager
+    {
+        private static readonly Microsoft.Extensions.Logging.ILogger GlobalLogger;
+        private static readonly ILoggerFactory LoggerFactory;
+        private static readonly CompositeDisposable Disposable = new CompositeDisposable();
+        static LogManager()
+        {
+            LoggerFactory = UnityLoggerFactory.Create(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Trace);
+                //Filter
+                //Set Option for logger
+                builder.AddZLoggerFile(
+                    @"C:\Users\kdahi\OneDrive\Documents\Unity\TechDemo\TechDemoProject\Assets\Log\EditorLog.log",
+                    options =>
+                    {
+                        var prefixFormat = ZString.PrepareUtf8<DateTime,LogLevel, DateTime, Exception>("[{0}]\n[{1}][{2}]{3}");
+                        options.PrefixFormatter = (writer, info) => prefixFormat.FormatTo(ref writer, DateTime.Now, info.LogLevel,
+                            info.Timestamp.Date.ToLocalTime(), info.Exception);
+
+
+                        var exceptionFormat =
+                            ZString.PrepareUtf8<DateTime ,string, string, MethodBase, Exception, string>(
+                                " [{0}] \n\n[{1}][{2}][{3}][{4}] {5}");
+                        options.ExceptionFormatter = (writer, exception) => exceptionFormat.FormatTo(ref writer, DateTime.Now, 
+                            exception.Source, exception.Message, exception.TargetSite, exception.InnerException,
+                            exception.StackTrace);
+
+                        // options.StructuredLoggingFormatter = (writer, info) => { };
+                        // options.EnableStructuredLogging = true;
+                        
+                        options.JsonSerializerOptions.WriteIndented = true;
+
+                    });
+                
+                builder.AddZLoggerUnityDebug(options =>
+                {
+                    var prefixFormat = ZString.PrepareUtf8<LogLevel, DateTime>("[{0}][{1}]");
+                    options.PrefixFormatter = (writer, info) => prefixFormat.FormatTo(ref writer, info.LogLevel, info.Timestamp.DateTime.ToLocalTime());
+                    
+                    var exceptionFormat =
+                        ZString.PrepareUtf8<string, string, MethodBase, Exception, string>(
+                            "[{0}][{1}][{2}][{3}] {4}");
+                    options.ExceptionFormatter = (writer, exception) => exceptionFormat.FormatTo(ref writer,
+                        exception.Source, exception.Message, exception.TargetSite, exception.InnerException,
+                        exception.StackTrace); 
+                    
+                    options.EnableStructuredLogging = true;
+                });
+            });
+
+            GlobalLogger = LoggerFactory.CreateLogger("Global");
+
+            Application.quitting += () =>
+            {
+                Logger.ZLog(LogLevel.Information, "Closing Logger and Disposing");
+                LoggerFactory.Dispose();
+            };
+        }
+        
+        public static Microsoft.Extensions.Logging.ILogger Logger => GlobalLogger;
+        public static ILogger<T> GetLogger<T>() where T : class => LoggerFactory.CreateLogger<T>();
+
+        public static Microsoft.Extensions.Logging.ILogger GetLogger(string category) =>
+            LoggerFactory.CreateLogger(category);
+    }
+}
