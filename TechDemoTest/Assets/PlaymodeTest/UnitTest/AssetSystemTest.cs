c73e215291fb786fc2acb13bd48d65c16c474cc7
KDahir247 <k.dahir243@hotmail.com>
2020-12-28 16:51:18 -0600

    Cleaned and optimized asset system by following Clean Code principle & compacted class
    Cleaned class Asset System and optimized gc calls by reducing gc calls and reducing ms. Followed principle from Clean Code book.
    Method should have less then 3 parameter and other principles.

--- TechDemoTest/Assets/PlaymodeTest/UnitTest/AssetSystemTest.cs
+++ TechDemoTest/Assets/PlaymodeTest/UnitTest/AssetSystemTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using NUnit.Framework;
@@ -11,57 +12,64 @@ namespace Tech.Test
 {
     public class AssetSystemTest
     {
+        private readonly List<GameObject> entityContainerLoop = new List<GameObject>(1);
         private AssetSystem<GameObject> _assetSystem;
-        private EntityInfo _entityInfo;
-        readonly List<GameObject> entityContainerLoop = new List<GameObject>(1);
+        private AssetSystem<GameObject> _assetSystemCanceled;
+
+
+        private AssetInfo assetInfo;
 
         [SetUp]
         public void AssetSystemSetUp()
         {
-            _assetSystem = new AssetSystem<GameObject>();
-            _entityInfo = new EntityInfo("Cube", new InstantiationParameters(Vector3.zero, Quaternion.identity, null));
+            _assetSystem = new AssetSystem<GameObject>(CancellationToken.None);
+            assetInfo = new AssetInfo("Cube", new InstantiationParameters(Vector3.zero, Quaternion.identity, null));
+
+            var cts = new CancellationTokenSource();
+
+            cts.Cancel();
+
+            _assetSystemCanceled = new AssetSystem<GameObject>(cts.Token);
         }
 
-        [Test, Performance]
+        [Test]
+        [Performance]
         public void AssetSystemCanceledTestSimplePasses()
-        { 
-            List<GameObject> entityContainer = new List<GameObject>(1);
+        {
+            var entityContainer = new List<GameObject>(1);
 
-            CancellationTokenSource cts = new CancellationTokenSource();
-            
-            cts.Cancel();
-            
-            Assert.DoesNotThrow( async () =>
+            Assert.Throws<OperationCanceledException>(() =>
             {
                 using (Measure.Scope("Loading Asset Canceled"))
                 {
-                    _assetSystem.LoadAsset(_entityInfo, entityContainer, null, cts.Token).Forget();
+                    _assetSystemCanceled.LoadAsset(assetInfo, entityContainer);
                 }
             });
         }
 
         // A UnityTest behaves like a coroutine in PlayMode
         // and allows you to yield null to skip a frame in EditMode
-        [UnityTest, Performance]
+        [UnityTest]
+        [Performance]
         public IEnumerator AssetSystemTestWithEnumeratorPasses()
         {
             // Use the Assert class to test conditions.
             // yield to skip a frame
-            
+
             //Multiple Instantiation average of 10 instantiation call
-            Assert.DoesNotThrow(() => 
+            Assert.DoesNotThrow(() =>
                 Measure
-                .Method(() => _assetSystem.LoadAsset(_entityInfo, entityContainerLoop, null, CancellationToken.None).Forget())
-                .SampleGroup("Loading Asset Iteration")
-                .GC()
-                .Run());
+                    .Method(() => _assetSystem.LoadAsset(assetInfo, entityContainerLoop))
+                    .SampleGroup("Loading Asset Iteration")
+                    .GC()
+                    .Run());
 
             yield return Measure.Frames()
                 .MeasurementCount(20)
                 .SampleGroup("Frame Performance Measurement")
                 .Run();
 
-            
+
             using (Measure.Scope("Unloading All Asset"))
             {
                 _assetSystem.UnloadAllAsset(entityContainerLoop);
--- TechDemoTest/Assets/Project/Scripts/Core/System/AssetSystem.cs
+++ TechDemoTest/Assets/Project/Scripts/Core/System/AssetSystem.cs
@@ -1,68 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.ResourceLocations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 
-public class EntityInfo
+public readonly struct AssetInfo
 {
-    public EntityInfo(string name, InstantiationParameters instantiationParameters)
+    internal readonly string Name;
+    internal readonly InstantiationParameters InstantiationParameters;
+
+    public AssetInfo(string name, InstantiationParameters instantiationParameters)
     {
         Name = name;
         InstantiationParameters = instantiationParameters;
     }
-
-    public readonly string Name;
-    public readonly InstantiationParameters InstantiationParameters;
 }
 
 //TODO include Custom Logger
 public sealed class AssetSystem<T>
-    where T : UnityEngine.Object
+    where T : Object
 {
-    private UniTask<GameObject>[] _taskToComplete;
-    
-    public async UniTaskVoid LoadAsset([NotNull] EntityInfo entityInfo,
-        IList<T> instantiatedContainer,
-        IProgress<float> progress,
-        CancellationToken cancellationToken)
+    private CancellationToken cancellationToken;
+
+    public AssetSystem(CancellationToken token)
+    {
+        cancellationToken = token;
+    }
+
+    public void LoadAsset(AssetInfo assetInfo,
+        [NotNull] IList<T> loadAssetContainer)
     {
         cancellationToken.Register(OperationCanceled);
-        
         cancellationToken.ThrowIfCancellationRequested();
-        
-        IList<IResourceLocation> resourceLocations = await Addressables
-            .LoadResourceLocationsAsync(entityInfo.Name)
-            .ToUniTask(progress, PlayerLoopTiming.Update, cancellationToken);
 
-
-        _taskToComplete = new UniTask<GameObject>[resourceLocations.Count];
-        
-        for (int i = 0; i < resourceLocations.Count; i++)
-        {
-            _taskToComplete[i] = Addressables.InstantiateAsync(resourceLocations[i], entityInfo.InstantiationParameters)
-                .ToUniTask(progress, PlayerLoopTiming.Update, cancellationToken);
-        }
-
-        GameObject[] results = await UniTask.WhenAll(_taskToComplete);
-
-        for (byte index = 0; index < results.Length; index++)
-        {
-            var gameObject = results[index];
-            instantiatedContainer.Add(gameObject as T);
-        }
+        InstantiateAsset(assetInfo, loadAssetContainer).Forget();
     }
-    
+
+    private async UniTaskVoid InstantiateAsset(AssetInfo assetInfo,
+        [NotNull] IList<T> loadAssetContainer)
+    {
+        var resourceLocations = await Addressables
+            .LoadResourceLocationsAsync(assetInfo.Name)
+            .ToUniTask(cancellationToken: cancellationToken);
+
+        var gameObject = await Addressables.InstantiateAsync(resourceLocations[0], assetInfo.InstantiationParameters)
+            .ToUniTask(cancellationToken: cancellationToken);
+
+        loadAssetContainer.Add(gameObject as T);
+    }
+
     public void UnloadAllAsset([NotNull] IEnumerable<T> releaseObjs)
     {
-        foreach (var releaseObj in releaseObjs)
-        {
-            Addressables.Release(releaseObj);
-        }
+        foreach (var releaseObj in releaseObjs) Addressables.Release(releaseObj);
     }
 
     private void OperationCanceled()
