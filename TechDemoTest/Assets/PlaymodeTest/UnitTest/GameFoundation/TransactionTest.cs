dedb1721210d18c200ec6aa2dd9825ab1f247b38
KDahir247 <k.dahir243@hotmail.com>
2021-01-04 17:38:19 -0600

    Created Transaction System and tested
    The transaction system has been created and tested it work with the wallet system, inventory system without know about each other.
    
    Transaction will see if player has sufficient cost and will handle the virtual transaction and give the player the payout if it is a success and from there it will add/remove it to the inventory system and wallet system.
    
    Transaction will be refactor as development continues and re tested

---
+++ TechDemoTest/Assets/PlaymodeTest/UnitTest/GameFoundation/TransactionTest.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using UniRx;
+using Unity.PerformanceTesting;
+using UnityEngine;
+
+namespace Tech.Test
+{
+    public sealed class TransactionTest
+    {
+        private GameInventory _gameInventory;
+        private GameTransaction _gameTransaction;
+        private GameWallet _wallet;
+
+        [SetUp]
+        public void TransactionTestSetUp()
+        {
+            Object.Instantiate(Resources.Load<GameObject>("Prefabs/GameFoundation"));
+
+            _wallet = new GameWallet();
+            _gameInventory = new GameInventory();
+            _gameTransaction = new GameTransaction();
+        }
+
+        [Test]
+        [Performance]
+        public void TransactionFullTestSimplePasses()
+        {
+            // Use the Assert class to test conditions.
+
+            Assert.DoesNotThrow(() =>
+            {
+                using (Measure.Scope("Full Game Foundation Integration"))
+                {
+                    _wallet.AddToWallet("fakeMoney", 1000);
+                    _wallet.WalletValueChanged().Subscribe(val => Debug.Log($"Cash amount is now {val.quantity}"));
+
+                    _gameInventory.InventoryValueChanged()
+                        .Subscribe(val => Debug.Log($"bought a {val.definition.displayName}"));
+
+                    _gameTransaction.OnTransactionCompleted().Subscribe(_ => Debug.Log("transaction Completed"));
+                    _gameTransaction.OnTransactionInitiated()
+                        .Subscribe(_ => Debug.Log($"Transaction has started for {_.displayName}"));
+                    _gameTransaction.OnTransactionProgress().Subscribe(tuple =>
+                        Debug.Log($"current step {tuple.Item1} total steps left {tuple.Item2}"));
+                    _gameTransaction.OnTransactionFailed()
+                        .Subscribe(exception => Debug.Log(exception?.InnerException.Message));
+
+                    _gameTransaction.Purchase("rockBlueprint");
+                    _gameTransaction.Purchase("rockBlueprint");
+                    _gameTransaction.Purchase("rockBlueprint");
+                }
+            });
+        }
+
+        [Test]
+        [Performance]
+        public void TransactionSuccessTestSimplePasses()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                _wallet.AddToWallet("fakeMoney", 200000);
+
+                Measure.Method(() => _gameTransaction.Purchase("rockBlueprint"))
+                    .SampleGroup("Transaction Success Purchase")
+                    .WarmupCount(5)
+                    .IterationsPerMeasurement(20)
+                    .MeasurementCount(10)
+                    .GC()
+                    .Run();
+            });
+        }
+
+        [Test]
+        [Performance]
+        public void TransactionFailedTestSimplePasses()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                _wallet.ResetWalletAmount("fakeMoney");
+
+                Measure.Method(() => _gameTransaction.Purchase("rockBlueprint"))
+                    .SampleGroup("Transaction Failed Purchase")
+                    .WarmupCount(5)
+                    .IterationsPerMeasurement(20)
+                    .MeasurementCount(10)
+                    .GC()
+                    .Run();
+            });
+        }
+
+        [TearDown]
+        public void TransactionTestTearDown()
+        {
+            _gameInventory.Dispose();
+            _wallet.Dispose();
+            _gameTransaction.Dispose();
+        }
+    }
+}
---
+++ TechDemoTest/Assets/Project/Scripts/GameFoundation/TransactionSystem/GameTransaction.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+using UniRx;
+using UnityEngine.GameFoundation;
+using ZLogger;
+
+public sealed class GameTransaction : DataFoundation
+{
+    private readonly CompositeDisposable _disposable
+        = new CompositeDisposable();
+
+    private readonly ILogger _logger
+        = LogManager.GetLogger<GameTransaction>();
+
+    private readonly Subject<TransactionResult> _transactionCompletedSubject
+        = new Subject<TransactionResult>();
+
+    private readonly Subject<Exception> _transactionFailedSubject
+        = new Subject<Exception>();
+
+    private readonly Subject<BaseTransaction> _transactionInitializedSubject
+        = new Subject<BaseTransaction>();
+
+    private readonly Subject<(int, int)> _transactionStepSubject
+        = new Subject<(int, int)>();
+
+    private readonly Dictionary<string, VirtualTransaction> _virtualDictionary
+        = new Dictionary<string, VirtualTransaction>();
+
+    private readonly List<VirtualTransaction> _virtualTransactions
+        = new List<VirtualTransaction>();
+
+    public GameTransaction()
+    {
+        try
+        {
+            ReceiveTransactionData();
+            SubscribeToGameFoundationEvent();
+        }
+        catch (Exception e)
+        {
+            _logger.ZLogCritical(e.Message);
+            throw new Exception(e.Message);
+        }
+    }
+
+    private void ReceiveTransactionData()
+    {
+        GameFoundationSdk.catalog.GetItems(_virtualTransactions);
+
+        if (_virtualTransactions.Count <= 0) return;
+
+        for (byte i = 0; i < _virtualTransactions.Count; i++)
+            _virtualDictionary.Add(_virtualTransactions[i].key, _virtualTransactions[i]);
+    }
+
+    public void Purchase([NotNull] string virtualTransactionKey)
+    {
+        if (!_virtualDictionary.ContainsKey(virtualTransactionKey)) return;
+
+        if (!MainThreadDispatcher.IsInitialized)
+            MainThreadDispatcher.Initialize();
+
+        MainThreadDispatcher
+            .StartCoroutine(ExecuteTransaction(_virtualDictionary[virtualTransactionKey]));
+    }
+
+    private IEnumerator ExecuteTransaction([NotNull] BaseTransaction virtualTransaction)
+    {
+        using var deferred = GameFoundationSdk.transactions.BeginTransaction(virtualTransaction);
+        while (!deferred.isDone) yield return null;
+    }
+
+    public override void Dispose()
+    {
+        UnSubscribeToGameFoundationEvent();
+
+        _transactionInitializedSubject.Dispose();
+        _transactionStepSubject.Dispose();
+        _transactionCompletedSubject.Dispose();
+        _transactionFailedSubject.Dispose();
+
+        if (!_disposable.IsDisposed)
+            _disposable.Dispose();
+    }
+
+    protected override void SubscribeToGameFoundationEvent()
+    {
+        GameFoundationSdk.transactions.transactionInitiated += TransactionInitiated;
+        GameFoundationSdk.transactions.transactionProgressed += TransactionProgressing;
+        GameFoundationSdk.transactions.transactionFailed += TransactionFailed;
+        GameFoundationSdk.transactions.transactionSucceeded += TransactionCompleted;
+    }
+
+    protected override void UnSubscribeToGameFoundationEvent()
+    {
+        GameFoundationSdk.transactions.transactionInitiated -= TransactionInitiated;
+        GameFoundationSdk.transactions.transactionProgressed -= TransactionProgressing;
+        GameFoundationSdk.transactions.transactionFailed -= TransactionFailed;
+        GameFoundationSdk.transactions.transactionSucceeded -= TransactionCompleted;
+    }
+
+    public IObservable<TransactionResult> OnTransactionCompleted()
+    {
+        return _transactionCompletedSubject
+            .AddTo(_disposable)
+            .AsObservable();
+    }
+
+    public IObservable<Exception> OnTransactionFailed()
+    {
+        return _transactionFailedSubject
+            .AddTo(_disposable)
+            .AsObservable();
+    }
+
+    public IObservable<(int, int)> OnTransactionProgress()
+    {
+        return _transactionStepSubject
+            .AddTo(_disposable)
+            .AsObservable();
+    }
+
+    public IObservable<BaseTransaction> OnTransactionInitiated()
+    {
+        return _transactionInitializedSubject
+            .AddTo(_disposable)
+            .AsObservable();
+    }
+
+    private void TransactionCompleted(BaseTransaction transaction, TransactionResult transactionResult)
+    {
+        _transactionCompletedSubject.OnNext(transactionResult);
+    }
+
+    private void TransactionFailed(BaseTransaction transaction, Exception exceptionThrown)
+    {
+        _transactionFailedSubject.OnNext(exceptionThrown);
+    }
+
+    private void TransactionProgressing(BaseTransaction transaction, int currentStep, int totalStep)
+    {
+        _transactionStepSubject.OnNext((currentStep, totalStep));
+    }
+
+    private void TransactionInitiated(BaseTransaction transaction)
+    {
+        _transactionInitializedSubject.OnNext(transaction);
+    }
+}
